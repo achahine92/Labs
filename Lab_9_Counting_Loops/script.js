eae034c160e241a28852f86df5cca95d258dbbfe
Amanda Chahine <hello@grandcircus.co>
2024-11-06 23:49:07 -0500

    updating Lab 9

--- Lab_9_Counting_Loops/script.js
+++ Lab_9_Counting_Loops/script.js
@@ -1,35 +1,46 @@
 for (let i = 1; i <= 10; i++) {
-    console.log(i);
+    console.log(i)
 }
 
 for (let i = 10; i >= 1; i--) {
-    console.log(i);
+    console.log(i)
 }
 
-let i = 0;
+let i = 0
 while (i < 10) {
-    i++;
-    console.log(i);
+    i++
+    console.log(i)
 }
 
-i = 10;
+i = 10
 while (i >= 1) {
-    console.log(i);
-    i--;
+    console.log(i)
+    i--
 }
 
 do {
-    i++;
-    console.log(i);
-} while (i < 10);
+    i++
+    console.log(i)
+} while (i < 10)
 
-i = 10;
+i = 10
 do {
-    console.log(i);
-    i--;
-} while (i >= 1);
+    console.log(i)
+    i--
+} while (i >= 1)
 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for (const number of numbers) {
-    console.log(number);
+    console.log(number)
 }
+
+// Extended Challenge
+
+function paddingStringsToTen(anyString) {
+    while (anyString.length < 10) {
+        anyString = " " + anyString
+    }
+    return anyString
+}
+
+console.log(paddingStringsToTen("Hi"))
