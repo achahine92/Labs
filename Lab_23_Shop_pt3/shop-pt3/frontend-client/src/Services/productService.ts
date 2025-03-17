f0df84f161b2d2e81f3de457f6fe822771adf910
Ryan Hall <125756539+dbcc0de@users.noreply.github.com>
2025-03-17 19:36:06 -0400

    Update productService.ts

--- Lab_23_Shop_pt3/shop-pt3/frontend-client/src/Services/productService.ts
+++ Lab_23_Shop_pt3/shop-pt3/frontend-client/src/Services/productService.ts
@@ -2,6 +2,8 @@ import Product from "../Models/Product"
 import axios from "axios"
 import "../Images/coming-soon.jpg"
 
+// You'll want to try importing the base url like you would an API with an .env file
+// const baseUrl = import.meta.env.VITE_API_BASE_URL; // located in a env.local file
 const baseUrl = "http://localhost:3000"
 const imgUrl = "../Images/coming-soon.jpg"
 
